c2985b7c89baf544c8c1789783c26bc45633ee0e
paoloros97 <paolorosettani97@gmail.com>
2021-05-18 18:26:35 +0200

    Installazione pytorch in base all'hardware

--- __init__.py
+++ __init__.py
@@ -9,6 +9,7 @@ modulesNames = ['animazione']
  
 import sys
 import importlib
+import subprocess
 import bpy
 from bpy.utils import register_class, unregister_class
 import os
@@ -16,8 +17,8 @@ import os
 try: #Prova ad importare la libreria, altrimenti la installa
     import pandas as pd
 except ImportError:
-    import subprocess
-    import bpy 
+    #import subprocess
+    #import bpy 
     try:
         import ensurepip
         ensurepip.bootstrap()
@@ -29,27 +30,74 @@ except ImportError:
     import pandas as pd  
 
 
+
 try:
+    #import librosa
     import torch
+
 except ImportError:
-    import subprocess
-    import bpy 
+
     try:
-        import ensurepip
-        ensurepip.bootstrap()
-        os.environ.pop("PIP_REQ_TRACKER", None)
-    except ImportError:
-        pass    
-    pybin = bpy.app.binary_path_python
-    subprocess.check_call([pybin,'-m','pip','install','torch==1.8.1+cu111', '-f', 'https://download.pytorch.org/whl/torch_stable.html'])
-    import torch
+        sp = subprocess.Popen(['nvidia-smi', '-q'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+        out_str = sp.communicate()
+        out_list = out_str[0].decode("utf-8").split('\n')
+
+        out_dict = {}
+
+        for item in out_list:
+            try:
+                key, val = item.split(':')
+                key, val = key.strip(), val.strip()
+                out_dict[key] = val
+            except:
+                pass
+        has_CUDA = False
+        try:
+            has_CUDA = 'CUDA Version' in out_dict
+        except:
+            pass
+        if has_CUDA:
+            try:
+                import ensurepip
+                ensurepip.bootstrap()
+                os.environ.pop("PIP_REQ_TRACKER", None)
+            except ImportError:
+                pass    
+            pybin = bpy.app.binary_path_python
+            
+            if float(out_dict['CUDA Version']) < 11.0 and float(out_dict['CUDA Version']) >= 10.0:
+                print('Installazione PyTorch con CUDA v10')
+                #subprocess.check_call([pybin,'-m','pip','install','torch==1.8.1+cu102', '-f', 'https://download.pytorch.org/whl/torch_stable.html'])
+            elif float(out_dict['CUDA Version']) >= 11.0:
+                print('Installazione PyTorch con CUDA v11')
+                #subprocess.check_call([pybin,'-m','pip','install','torch==1.8.1+cu111', '-f', 'https://download.pytorch.org/whl/torch_stable.html'])
+            else:
+                print('Installazione PyTorch CPU (CUDA presente ma con versione non disponibile)')
+                #subprocess.check_call([pybin,'-m','pip','install','torch==1.8.1+cpu', '-f', 'https://download.pytorch.org/whl/torch_stable.html'])
+
+            import torch
+        else:
+            raise Exception()
+ 
+    except:
+        try:
+            import ensurepip
+            ensurepip.bootstrap()
+            os.environ.pop("PIP_REQ_TRACKER", None)
+        except ImportError:
+            pass    
+        pybin = bpy.app.binary_path_python
+        print('Installazione PyTorch CPU')
+        #subprocess.check_call([pybin,'-m','pip','install','torch==1.8.1+cpu', '-f', 'https://download.pytorch.org/whl/torch_stable.html'])
+        import torch
 
 
 try:
     import scipy.io as sio
 except ImportError:
-    import subprocess
-    import bpy 
+    #import subprocess
+    #import bpy 
     try:
         import ensurepip
         ensurepip.bootstrap()
@@ -63,8 +111,8 @@ except ImportError:
 try:
     import joblib
 except ImportError:
-    import subprocess
-    import bpy 
+    #import subprocess
+    #import bpy 
     try:
         import ensurepip
         ensurepip.bootstrap()
